669b6f2d9ff4dba38e0798023dcaaab2b13bb872
ericmreid <ereid08@gmail.com>
2018-01-31 17:42:15 -0500

    styles

--- routes/sunny.js
+++ routes/sunny.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('sunny/sunnyifier', { title: "It's Always Sunny-ifier" });
+  res.render('sunny/index', { title: "It's Always Sunny-ifier" });
   // res.json({thing: 'hey'});
 });
 
